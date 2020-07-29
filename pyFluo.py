ba890289e16c23b72562fab6a5e2b2ea96829fd3
Federico Ceriani <f.ceriani@sheffield.ac.uk>
2020-07-29 11:34:22 +0100

    Added event time limit and button to interface. To activate/deactivate event the number 5 is sent to arduino

--- pyFluo.py
+++ pyFluo.py
@@ -19,7 +19,9 @@ class mainWindow(QMainWindow, UI_MainWindow):
         self.exp2 = 15
         self.isi = 100
         self.ifi = 1000
-        self.com = 'COM5'
+        self.com = 'COM6'
+        self.eventStart = 0
+        self.eventEnd = 0
         # Try to open serial with defaul value
         try:
             self.ser = serial.Serial(self.com,9600, timeout=5, parity=serial.PARITY_EVEN, rtscts=1)
@@ -69,27 +71,37 @@ class mainWindow(QMainWindow, UI_MainWindow):
     def on_isiSpin_valueChanged(self, value):
         self.isi = value
 
-
-
-
-
     @pyqtSlot(int)
     def on_ifiSpin_valueChanged(self, value):
         self.ifi = value
 
+    @pyqtSlot(int)
+    def on_eventStartSpinBox_valueChanged(self, value):
+        self.eventStart = value
+
+    @pyqtSlot(int)
+    def on_eventEndSpinBox_valueChanged(self, value):
+        self.eventEnd = value
+
+
     @pyqtSlot(bool)
     def on_Led1PushButton_clicked(self):
-        message = "3 0 0 0 0 "
+        message = "3 0 0 0 0 0 0 "
         self.sendMessage(message)
 
     @pyqtSlot(bool)
     def on_Led2PushButton_clicked(self):
-        message = "4 0 0 0 0 "
+        message = "4 0 0 0 0 0 0 "
+        self.sendMessage(message)
+
+    @pyqtSlot(bool)
+    def on_eventpushButton_clicked(self):
+        message = "5 0 0 0 0 0 0 "
         self.sendMessage(message)
 
     @pyqtSlot(bool)
     def on_blinkPushButton_clicked(self):
-        message = "2 "+str(self.exp1)+" "+str(self.exp2)+ " " + str(self.isi) + " " + str(self.ifi) + " "
+        message = "2 "+str(self.exp1)+" "+str(self.exp2)+ " " + str(self.isi) + " " + str(self.ifi) + " " + str(self.eventStart) + " " + str(self.eventEnd) + " "
         self.sendMessage(message)
 
     def sendMessage(self, message):
